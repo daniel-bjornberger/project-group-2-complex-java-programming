3a6bf7255cbc05d76c287076f8529390d0199fcc
Elin <elin.sexton@gmail.com>
2019-11-26 14:09:08 +0100

    Add variables to Monster class.

--- src/main/java/se/iths/complexjavaproject/mudders/model/Monster.java
+++ src/main/java/se/iths/complexjavaproject/mudders/model/Monster.java
@@ -1,4 +1,50 @@
 package se.iths.complexjavaproject.mudders.model;
 
-public class Monster {
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="monster")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Monster implements Serializable {
+
+    private static final long serialVersionUID = -2799455774396393279L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="monsterType")
+    private String monsterType;
+
+    @Column(name="level")
+    private int level;
+
+    @Column(name="manaPoints")
+    private int manaPoints;
+
+    @Column(name="healthPoints")
+    private int healthPoints;
+
+    @Column(name="attackPoints")
+    private int attackPoints;
+
+    //experience points given to player after monster is defeated.
+    @Column(name="experience")
+    private int experience;
+
+    //private List<Loot
+    // > monsterLoot;
+
+    public Monster toEntity(){
+
+        return null;
+
+    }
+
 }
