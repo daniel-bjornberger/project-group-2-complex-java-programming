e77a5578052043957f312dba6bb43599e8ab9c07
Daniel Björnberger <daniel.bjornberger@gmail.com>
2019-11-26 11:43:50 +0000

    First commit

---
+++ src/main/java/se/iths/complexjavaproject/mudders/MuddersApplication.java
@@ -0,0 +1,13 @@
+package se.iths.complexjavaproject.mudders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuddersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuddersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/iths/complexjavaproject/mudders/MuddersApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.complexjavaproject.mudders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuddersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
