ccc753f65e7078d3702e6cb3ff53b5ff44d12e59
Elin <elin.sexton@gmail.com>
2019-11-26 14:23:28 +0100

    Add monsterModel

--- src/main/java/se/iths/complexjavaproject/mudders/dto/MonsterModel.java
+++ src/main/java/se/iths/complexjavaproject/mudders/dto/MonsterModel.java
@@ -1,4 +1,37 @@
 package se.iths.complexjavaproject.mudders.dto;
 
+import lombok.*;
+import se.iths.complexjavaproject.mudders.model.Monster;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class MonsterModel {
+
+    private Long id;
+    private String monsterType;
+    private int level;
+    private int manaPoints;
+    private int healthPoints;
+    private int attackPoints;
+    //experience points given to player after monster is defeated.
+    private int experience;
+
+    public MonsterModel toModel(Monster monster) {
+        MonsterModel monsterModel = new MonsterModel();
+
+        monsterModel.setMonsterType(monster.getMonsterType());
+        monsterModel.setLevel(monster.getLevel());
+        monsterModel.setHealthPoints(monster.getHealthPoints());
+        monsterModel.setManaPoints(monster.getManaPoints());
+        monsterModel.setAttackPoints(monster.getAttackPoints());
+        monsterModel.setExperience(monster.getExperience());
+
+        return monsterModel;
+    }
+
+
+
 }
