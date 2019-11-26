3492f8e30e945085aca2d7a079588a858b3dcc3d
Filip Christofferson <filip.christofferson@gmail.com>
2019-11-26 14:15:57 +0100

    Create PlayerCharacterRepository

--- src/main/java/se/iths/complexjavaproject/mudders/dto/PlayerCharacterModel.java
+++ src/main/java/se/iths/complexjavaproject/mudders/dto/PlayerCharacterModel.java
@@ -5,13 +5,22 @@ import se.iths.complexjavaproject.mudders.model.PlayerCharacter;
 public class PlayerCharacterModel {
 
     private Long id;
-    private String name;
+    private String characterName;
     private int experience;
+    private int health;
+    private int mana;
+    private int level;
 
     public PlayerCharacterModel toDto(PlayerCharacter playerCharacter) {
         PlayerCharacterModel playerCharacterModel = new PlayerCharacterModel();
 
-        playerCharacterModel.
+        playerCharacterModel.id = null;
+        playerCharacterModel.characterName = playerCharacter.getCharacterName();
+        playerCharacterModel.experience = playerCharacter.getExperience();
+        playerCharacterModel.level = playerCharacter.getLevel();
+        playerCharacterModel.health = playerCharacter.getHealth();
+        playerCharacterModel.mana = playerCharacter.getMana();
 
+        return playerCharacterModel;
     }
 }
