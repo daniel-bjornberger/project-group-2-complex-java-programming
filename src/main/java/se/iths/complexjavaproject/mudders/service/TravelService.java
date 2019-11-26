2821f208b4ee327d941b66544831761f91893540
Elin <elin.sexton@gmail.com>
2019-11-26 16:05:29 +0100

    Add TravelService

---
+++ src/main/java/se/iths/complexjavaproject/mudders/service/TravelService.java
@@ -0,0 +1,17 @@
+package se.iths.complexjavaproject.mudders.service;
+
+/**
+ * Skapad av Elin och Tonny.
+ */
+public class TravelService {
+
+    public void daysToTown(){
+        //List of towns, days to town corresponds to index.
+    }
+
+    public void travel(){
+        //Travelling to next town.
+        //might be ambushed
+        //might find pot of gold
+    }
+}
