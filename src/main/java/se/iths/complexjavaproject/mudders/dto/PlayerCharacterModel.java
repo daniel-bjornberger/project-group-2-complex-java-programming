2282b22f87781e603fb4d02e3893d758a5d27bca
Filip Christofferson <filip.christofferson@gmail.com>
2019-11-26 14:10:30 +0100

    Add playerCharacterModel

--- src/main/java/se/iths/complexjavaproject/mudders/dto/PlayerCharacterModel.java
+++ src/main/java/se/iths/complexjavaproject/mudders/dto/PlayerCharacterModel.java
@@ -4,7 +4,14 @@ import se.iths.complexjavaproject.mudders.model.PlayerCharacter;
 
 public class PlayerCharacterModel {
 
+    private Long id;
+    private String name;
+    private int experience;
+
     public PlayerCharacterModel toDto(PlayerCharacter playerCharacter) {
+        PlayerCharacterModel playerCharacterModel = new PlayerCharacterModel();
+
+        playerCharacterModel.
 
     }
 }
