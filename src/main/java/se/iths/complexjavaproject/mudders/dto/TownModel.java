434b09488aa02cecc1d866a3288e6a6f92d63194
Elin <elin.sexton@gmail.com>
2019-11-28 11:50:56 +0100

    Add Town and TownModel

---
+++ src/main/java/se/iths/complexjavaproject/mudders/dto/TownModel.java
@@ -0,0 +1,24 @@
+package se.iths.complexjavaproject.mudders.dto;
+
+
+import lombok.*;
+import se.iths.complexjavaproject.mudders.model.Town;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class TownModel {
+
+    private Long id;
+    private String name;
+
+    public TownModel toDTO(Town town){
+        TownModel townModel = new TownModel();
+
+        townModel.setName(town.getName());
+        
+        return townModel;
+    }
+}
