83756da16bc270f7cd94d53fc70a7bfa4404d67d
Elin <elin.sexton@gmail.com>
2019-11-28 11:51:55 +0100

    Remove randomNumberGenerator()

--- src/main/java/se/iths/complexjavaproject/mudders/service/CombatService.java
+++ src/main/java/se/iths/complexjavaproject/mudders/service/CombatService.java
@@ -44,17 +44,5 @@ public class CombatService implements ICombatActions {
         return playerDamage;
     }*/
 
-    public double randomNumberGenerator(double min, double max){
-        double x = (int)(Math.random()*((max-min)+1))+ min;
-        return x;
 
-    }
-
-    public static int generateRandomIntIntRange(int min, int max) {
-
-        Random r = new Random();
-
-        return r.nextInt((max - min) + 1) + min;
-
-    }
 }
