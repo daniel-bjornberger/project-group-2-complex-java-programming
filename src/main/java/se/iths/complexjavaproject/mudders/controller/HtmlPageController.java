5ac1e2a532ce44aea274bcd79d0ed36545fc4c01
Daniel Björnberger <daniel.bjornberger@gmail.com>
2019-11-30 21:21:58 +0000

    Update the class 'HtmlPageController'

--- src/main/java/se/iths/complexjavaproject/mudders/controller/HtmlPageController.java
+++ src/main/java/se/iths/complexjavaproject/mudders/controller/HtmlPageController.java
@@ -1,9 +1,9 @@
 package se.iths.complexjavaproject.mudders.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HtmlPageController {
 
     @GetMapping("/")
