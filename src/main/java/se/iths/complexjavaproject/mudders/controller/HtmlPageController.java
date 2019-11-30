5e6efdcaf3d7f96b80213b2201fa54f3ee775ab1
Daniel Björnberger <daniel.bjornberger@gmail.com>
2019-11-30 20:13:26 +0000

    Write the method 'getLoginPage' in 'HtmlPageController'

--- src/main/java/se/iths/complexjavaproject/mudders/controller/HtmlPageController.java
+++ src/main/java/se/iths/complexjavaproject/mudders/controller/HtmlPageController.java
@@ -1,4 +1,14 @@
 package se.iths.complexjavaproject.mudders.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HtmlPageController {
+
+    @GetMapping("/")
+    public String getLoginPage() {
+        return "login";
+    }
+
 }
