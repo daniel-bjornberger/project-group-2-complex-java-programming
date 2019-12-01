c174bb5499737de4ab4ec309046f298e8725e55c
Daniel Björnberger <daniel.bjornberger@gmail.com>
2019-12-01 15:50:34 +0000

    Do a minor update of the class 'ActionController'

--- src/main/java/se/iths/complexjavaproject/mudders/controller/ActionController.java
+++ src/main/java/se/iths/complexjavaproject/mudders/controller/ActionController.java
@@ -1,4 +1,25 @@
 package se.iths.complexjavaproject.mudders.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/action")
 public class ActionController {
+
+    @GetMapping("/rest/{userName}/{characterName}")
+    public String rest(@PathVariable("userName") String userName,
+                       @PathVariable("characterName") String characterName) {
+
+        return "rest";
+
+    }
+
+    // TODO: Vad behöver skickas med från klienten? userName och characterName?
+
+
+    // travel, attack, flee osv.
+
 }
