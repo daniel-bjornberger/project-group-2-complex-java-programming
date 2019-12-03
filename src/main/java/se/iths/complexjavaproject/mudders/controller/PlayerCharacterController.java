0b864a0cd69a408f7befd75a3055ca54cacb2c36
Elin <elin.sexton@gmail.com>
2019-12-03 11:15:55 +0100

    Add GET findAll

--- src/main/java/se/iths/complexjavaproject/mudders/controller/PlayerCharacterController.java
+++ src/main/java/se/iths/complexjavaproject/mudders/controller/PlayerCharacterController.java
@@ -7,8 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import se.iths.complexjavaproject.mudders.model.PlayerCharacterModel;
 import se.iths.complexjavaproject.mudders.exception.BadDataException;
+import se.iths.complexjavaproject.mudders.model.PlayerCharacter;
 import se.iths.complexjavaproject.mudders.repository.PlayerCharacterRepository;
 import se.iths.complexjavaproject.mudders.service.PlayerCharacterService;
+
+import java.util.List;
 
 @RestController
 @NoArgsConstructor
@@ -17,6 +20,17 @@ public class PlayerCharacterController {
 
     @Autowired
     private PlayerCharacterRepository playerCharacterRepository;
+
+    @GetMapping(path = "/all")
+    public ResponseEntity getAllPlayers() {
+        try{
+            Iterable<PlayerCharacter> findAllPlayers = playerCharacterRepository.findAll();
+            return ResponseEntity.ok().body(findAllPlayers);
+        }
+        catch(Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 
     @PostMapping(path = "/add")
     public ResponseEntity addNewPlayerCharacter (@RequestBody String characterName){
