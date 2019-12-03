f0e8bc1371778d87e016c964db08da4fb7861c2e
Elin <elin.sexton@gmail.com>
2019-12-03 12:38:42 +0100

    Add getAll in PlayerCharacterController

--- src/main/java/se/iths/complexjavaproject/mudders/controller/PlayerCharacterController.java
+++ src/main/java/se/iths/complexjavaproject/mudders/controller/PlayerCharacterController.java
@@ -5,9 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import se.iths.complexjavaproject.mudders.entity.PlayerCharacter;
 import se.iths.complexjavaproject.mudders.model.PlayerCharacterModel;
 import se.iths.complexjavaproject.mudders.exception.BadDataException;
-import se.iths.complexjavaproject.mudders.model.PlayerCharacter;
 import se.iths.complexjavaproject.mudders.repository.PlayerCharacterRepository;
 import se.iths.complexjavaproject.mudders.service.PlayerCharacterService;
 
@@ -39,6 +39,9 @@ public class PlayerCharacterController {
                     .save(PlayerCharacterService.convertToEntity(characterName))
                     .toModel();
 
+            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            System.out.println(playerCharacterModel.toJson(playerCharacterModel));
+            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
             return ResponseEntity
                     .status(HttpStatus.CREATED)
                     .body(playerCharacterModel.toJson(playerCharacterModel));
