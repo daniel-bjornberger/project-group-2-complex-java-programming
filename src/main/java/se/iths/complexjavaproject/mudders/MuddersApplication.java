35acdd122561519906751eaf7d637c6a09db54cd
Tonny Frisk <tonny035@hotmail.com>
2019-12-04 08:39:47 +0100

    implement StartupService

--- src/main/java/se/iths/complexjavaproject/mudders/MuddersApplication.java
+++ src/main/java/se/iths/complexjavaproject/mudders/MuddersApplication.java
@@ -4,14 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import se.iths.complexjavaproject.mudders.entity.NonPlayerCharacter;
-import se.iths.complexjavaproject.mudders.repository.NonPlayerCharacterRepository;
+import se.iths.complexjavaproject.mudders.service.StartupService;
 
 @SpringBootApplication
 public class MuddersApplication implements CommandLineRunner {
 
 	@Autowired
-	NonPlayerCharacterRepository NPCRepository;
+	StartupService startupService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MuddersApplication.class, args);
@@ -19,18 +18,6 @@ public class MuddersApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
-//		================= Create Towns and NPCs =================
-//		TODO: Make this a service
-		Iterable<NonPlayerCharacter> allNPCs = NPCRepository.findAll();
-		if (!allNPCs.iterator().hasNext()) {
-//			TODO: Create Towns
-//			TODO: Create NPCs
-
-//			TODO: Set child reference in parent entity
-//			TODO: Set parent reference in child entity
-
-//			TODO: Save parent reference
-		}
+		startupService.populateDbIfNeeded();
 	}
 }
