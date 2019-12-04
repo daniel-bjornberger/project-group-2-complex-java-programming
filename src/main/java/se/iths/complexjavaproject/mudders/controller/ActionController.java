e9bab8e2dd380f9eb27bcd87d298766876b4ecdb
Daniel Björnberger <daniel.bjornberger@gmail.com>
2019-12-04 12:29:28 +0000

    Continue to improve the class 'ActionController'

--- src/main/java/se/iths/complexjavaproject/mudders/controller/ActionController.java
+++ src/main/java/se/iths/complexjavaproject/mudders/controller/ActionController.java
@@ -1,42 +1,46 @@
 package se.iths.complexjavaproject.mudders.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import se.iths.complexjavaproject.mudders.service.CombatService;
 
 @RestController
 @RequestMapping("/action")
 public class ActionController {
 
-    @GetMapping("/rest/{characterName}")
-    public String rest(@PathVariable("characterName") String characterName) {
+    private CombatService combatService;
 
-        return "rest";
+    @GetMapping(value = "/rest/{characterName}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ActionControllerResponse rest(@PathVariable("characterName") String characterName) {
 
-    }
-
-
-    @GetMapping("/attack/{characterName}")
-    public String attack(@PathVariable("characterName") String characterName) {
-
-        return "attack";
+        return new ActionControllerResponse("rest, " + characterName, true, true, false, true);
 
     }
 
 
-    @GetMapping("/travel/{characterName}")
-    public String travel(@PathVariable("characterName") String characterName) {
+    @GetMapping(value = "/attack/{characterName}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ActionControllerResponse attack(@PathVariable("characterName") String characterName) {
 
-        return "travel";
+        return new ActionControllerResponse("attack, " + characterName, false, true, false, true);
 
     }
 
 
-    @GetMapping("/flee/{characterName}")
-    public String flee(@PathVariable("characterName") String characterName) {
+    @GetMapping(value = "/travel/{characterName}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ActionControllerResponse travel(@PathVariable("characterName") String characterName) {
 
-        return "flee";
+        return new ActionControllerResponse("travel, " + characterName, true, false, false, false);
+
+    }
+
+
+    @GetMapping(value = "/flee/{characterName}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ActionControllerResponse flee(@PathVariable("characterName") String characterName) {
+
+        return new ActionControllerResponse("flee, " + characterName, true, false, false, false);
 
     }
 
