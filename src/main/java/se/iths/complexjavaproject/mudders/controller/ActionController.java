7910d46bf20ce260983cbc39856fb081b9a52c6c
Daniel Björnberger <daniel.bjornberger@gmail.com>
2019-12-04 21:20:43 +0000

    Change from GetMapping to PostMapping for the methods in the class 'ActionController'

--- src/main/java/se/iths/complexjavaproject/mudders/controller/ActionController.java
+++ src/main/java/se/iths/complexjavaproject/mudders/controller/ActionController.java
@@ -1,10 +1,8 @@
 package se.iths.complexjavaproject.mudders.controller;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import se.iths.complexjavaproject.mudders.model.PlayerCharacterModel;
 import se.iths.complexjavaproject.mudders.service.CombatService;
 
 @RestController
@@ -13,34 +11,38 @@ public class ActionController {
 
     private CombatService combatService;
 
-    @GetMapping(value = "/rest/{characterName}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ActionControllerResponse rest(@PathVariable("characterName") String characterName) {
+    @PostMapping(value = "/rest", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ActionControllerResponse rest(@RequestBody PlayerCharacterModel playerCharacterModel) {
 
-        return new ActionControllerResponse("rest, " + characterName, true, true, false, true);
-
-    }
-
-
-    @GetMapping(value = "/attack/{characterName}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ActionControllerResponse attack(@PathVariable("characterName") String characterName) {
-
-        return new ActionControllerResponse("attack, " + characterName, false, true, false, true);
+        return new ActionControllerResponse("rest, " + playerCharacterModel.getCharacterName(),
+                true, true, false, true);
 
     }
 
 
-    @GetMapping(value = "/travel/{characterName}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ActionControllerResponse travel(@PathVariable("characterName") String characterName) {
+    @PostMapping(value = "/travel", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ActionControllerResponse travel(@RequestBody PlayerCharacterModel playerCharacterModel) {
 
-        return new ActionControllerResponse("travel, " + characterName, true, false, false, false);
+        return new ActionControllerResponse("travel, " + playerCharacterModel.getCharacterName(),
+                true, false, false, false);
 
     }
 
 
-    @GetMapping(value = "/flee/{characterName}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ActionControllerResponse flee(@PathVariable("characterName") String characterName) {
+    @PostMapping(value = "/attack", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ActionControllerResponse attack(@RequestBody PlayerCharacterModel playerCharacterModel) {
 
-        return new ActionControllerResponse("flee, " + characterName, true, false, false, false);
+        return new ActionControllerResponse("attack, " + playerCharacterModel.getCharacterName(),
+                false, true, false, true);
+
+    }
+
+
+    @PostMapping(value = "/flee", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ActionControllerResponse flee(@RequestBody PlayerCharacterModel playerCharacterModel) {
+
+        return new ActionControllerResponse("flee, " + playerCharacterModel.getCharacterName(),
+                true, false, false, false);
 
     }
 
