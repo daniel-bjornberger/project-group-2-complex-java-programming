18c30f36eefc95e2638d158756a4f3458ad6df83
Elin <elin.sexton@gmail.com>
2019-12-04 11:43:41 +0100

    Add @Transactional to delete

--- src/main/java/se/iths/complexjavaproject/mudders/repository/TownRepository.java
+++ src/main/java/se/iths/complexjavaproject/mudders/repository/TownRepository.java
@@ -2,6 +2,7 @@ package se.iths.complexjavaproject.mudders.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import se.iths.complexjavaproject.mudders.entity.Town;
 
 @Repository
@@ -11,5 +12,6 @@ public interface TownRepository extends CrudRepository<Town, Long> {
 
     Town findTownByName(String townName);
 
+    @Transactional
     void deleteTownByName(String townName);
 }
