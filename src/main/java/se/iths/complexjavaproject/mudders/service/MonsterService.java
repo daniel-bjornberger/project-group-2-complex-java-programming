e7b014b39042a7b536bd5d8d82be9797f23a1d83
Tonny Frisk <tonny035@hotmail.com>
2019-12-05 11:54:17 +0100

    update MonsterService

--- src/main/java/se/iths/complexjavaproject/mudders/service/MonsterService.java
+++ src/main/java/se/iths/complexjavaproject/mudders/service/MonsterService.java
@@ -1,39 +1,37 @@
 package se.iths.complexjavaproject.mudders.service;
 
 import org.springframework.stereotype.Service;
-import se.iths.complexjavaproject.mudders.entity.Monster;
+import se.iths.complexjavaproject.mudders.model.MonsterModel;
 import se.iths.complexjavaproject.mudders.util.ServiceUtilities;
 
 @Service
 public class MonsterService {
 
-    public static Monster createNewRandomMonster(int level) {
-        Monster monster = new Monster();
+    public static MonsterModel createNewRandomMonster(int level) {
+        String name = "";
+        int damage = 0, health = 0, givenExperience = 0;
 
         int randomNameInt = ServiceUtilities.generateRandomIntIntRange(1, 3);
         switch (randomNameInt) {
             case 1:
-                monster.setName("Skeleton");
-                monster.setLevel(level);
-                monster.setDamage(1);
-                monster.setHealth(ServiceUtilities.generateRandomIntIntRange(6,7));
-                monster.setGivenExperience(ServiceUtilities.generateRandomIntIntRange(3,5));
+                name = "Skeleton";
+                damage = 1;
+                health = ServiceUtilities.generateRandomIntIntRange(6,7);
+                givenExperience = ServiceUtilities.generateRandomIntIntRange(3,5);
                 break;
             case 2:
-                monster.setName("Zombie");
-                monster.setLevel(level);
-                monster.setDamage(1);
-                monster.setHealth(ServiceUtilities.generateRandomIntIntRange(7,8));
-                monster.setGivenExperience(ServiceUtilities.generateRandomIntIntRange(4,5));
+                name = "Zombie";
+                damage = 1;
+                health = ServiceUtilities.generateRandomIntIntRange(7,8);
+                givenExperience = ServiceUtilities.generateRandomIntIntRange(4,5);
                 break;
             case 3:
-                monster.setName("Wolf");
-                monster.setLevel(level);
-                monster.setDamage(1);
-                monster.setHealth(ServiceUtilities.generateRandomIntIntRange(4,5));
-                monster.setGivenExperience(ServiceUtilities.generateRandomIntIntRange(1,3));
+                name = "Wolf";
+                damage = 1;
+                health = ServiceUtilities.generateRandomIntIntRange(4,5);
+                givenExperience = ServiceUtilities.generateRandomIntIntRange(1,3);
                 break;
         }
-        return monster;
+        return new MonsterModel(name,level,health,damage,givenExperience);
     }
 }
