845f6b46317415cf1eaa8094d8d13b9ef0213b7f
Filip Christofferson <filip.christofferson@gmail.com>
2019-12-06 14:41:36 +0100

    Remove unnecessary Controllers refactor project

--- src/main/java/se/iths/complexjavaproject/mudders/service/CombatService.java
+++ src/main/java/se/iths/complexjavaproject/mudders/service/CombatService.java
@@ -13,18 +13,22 @@ public class CombatService {
 
     public void fight(PlayerCharacter player, MonsterModel monster) {
 //        Player attacks monster
-        monster.setHealth(attack(monster.getHealth(), player.getDamage()));
-        if (monster.getHealth() == 0) {
-            player.setExperience(player.getExperience() + monster.getGivenExperience());
-            System.out.println("=========== " + monster.getName() + " killed! ===========");
-            System.out.println("=========== " + monster.getGivenExperience() + " experience gained! ===========");
+        if(!player.isInCombat()){
+            player.setInCombat(true);
         }
-        else {
+            monster.setHealth(attack(monster.getHealth(), player.getDamage()));
+            System.out.println("!------------------Monster now has " + monster.getHealth() + " health-----------------------------------!");
+            if (monster.getHealth() == 0) {
+                player.setExperience(player.getExperience() + monster.getGivenExperience());
+                System.out.println("=========== " + monster.getName() + " killed! ===========");
+                System.out.println("=========== " + monster.getGivenExperience() + " experience gained! ===========");
+                player.setInCombat(false);
+            } else {
 //            Monster attacks player
-            player.setHealth(attack(player.getHealth(), monster.getDamage()));
-            if (player.getHealth() == 0) {
-                System.out.println("=========== You died! ===========");
-            }
+                player.setHealth(attack(player.getHealth(), monster.getDamage()));
+                if (player.getHealth() == 0) {
+                    System.out.println("=========== You died! ===========");
+                }
         }
     }
 
@@ -40,16 +44,6 @@ public class CombatService {
             return 0;
         }
         return healthLeft;
-    }
-
-    public PlayerCharacter monsterAttack(MonsterModel monster, PlayerCharacter playerCharacter){
-        playerCharacter.setHealth(playerCharacter.getHealth() - monster.getDamage());
-        return playerCharacter;
-    }
-
-    public MonsterModel playerAttack(MonsterModel monster, PlayerCharacter playerCharacter){
-        monster.setHealth(monster.getHealth() - playerCharacter.getDamage());
-        return monster;
     }
 
     //TODO: How to make it so correct user gets to use the method?
