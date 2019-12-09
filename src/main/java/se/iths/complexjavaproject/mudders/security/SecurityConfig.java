ff712165e48ea65e645db26bf9ef915659539e96
Daniel Björnberger <daniel.bjornberger@gmail.com>
2019-12-09 09:59:25 +0000

    Create the class 'SecurityConfig'

---
+++ src/main/java/se/iths/complexjavaproject/mudders/security/SecurityConfig.java
@@ -0,0 +1,54 @@
+package se.iths.complexjavaproject.mudders.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.authorizeRequests()
+                    .antMatchers("/login*").permitAll()
+                    .antMatchers("/admin/**").hasRole("ADMIN")
+                    .anyRequest().authenticated()
+                    .and()
+                .formLogin()
+                    .loginPage("/login")                                // TODO: Eller "/login.html"
+                    .loginProcessingUrl("/perform_login")
+                    .defaultSuccessUrl("/playercharacter", true)
+                    //.failureUrl("/login.html?error=true")
+                    .and()
+                .logout()
+                    .permitAll();
+
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+
+        authenticationManagerBuilder.inMemoryAuthentication()
+                .withUser("user1").password(passwordEncoder().encode("user1pw")).roles("USER")
+                .and()
+                .withUser("user2").password(passwordEncoder().encode("user2pw")).roles("USER")
+                .and()
+                .withUser("admin").password(passwordEncoder().encode("adminpw")).roles("USER", "ADMIN");
+
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
