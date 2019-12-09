fa0b6761aa8c6115f4f978e40c2659c9f215923f
Elin <elin.sexton@gmail.com>
2019-12-09 14:05:23 +0100

    Add fight(PlayerCharacter player, MonsterModel monster)

--- src/main/java/se/iths/complexjavaproject/mudders/service/World.java
+++ src/main/java/se/iths/complexjavaproject/mudders/service/World.java
@@ -3,6 +3,7 @@ package se.iths.complexjavaproject.mudders.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import se.iths.complexjavaproject.mudders.entity.PlayerCharacter;
 import se.iths.complexjavaproject.mudders.exception.BadDataException;
+import se.iths.complexjavaproject.mudders.model.MonsterModel;
 import se.iths.complexjavaproject.mudders.model.PlayerCharacterModel;
 import se.iths.complexjavaproject.mudders.repository.PlayerCharacterRepository;
 import se.iths.complexjavaproject.mudders.util.ServiceUtilities;
@@ -31,6 +32,23 @@ public class World {
         else {
             //might find pot of gold
             return travelService.potOfGold(playerCharacter).toModel();
+        }
+    }
+
+    public void fight(PlayerCharacter player, MonsterModel monster) {
+//        Player attacks monster
+        monster.setHealth(combatService.attack(monster.getHealth(), player.getDamage()));
+        if (monster.getHealth() == 0) {
+            player.setExperience(player.getExperience() + monster.getGivenExperience());
+            System.out.println("=========== " + monster.getName() + " killed! ===========");
+            System.out.println("=========== " + monster.getGivenExperience() + " experience gained! ===========");
+        }
+        else {
+//            Monster attacks player
+            player.setHealth(combatService.attack(player.getHealth(), monster.getDamage()));
+            if (player.getHealth() == 0) {
+                System.out.println("=========== You died! ===========");
+            }
         }
     }
 }
