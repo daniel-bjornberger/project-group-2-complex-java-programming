591a47571c9eb9527526ee17b15b5aaa62c15e49
Elin <elin.sexton@gmail.com>
2019-12-09 15:34:01 +0100

    Removed whit space

--- src/main/java/se/iths/complexjavaproject/mudders/repository/TownRepository.java
+++ src/main/java/se/iths/complexjavaproject/mudders/repository/TownRepository.java
@@ -8,8 +8,6 @@ import se.iths.complexjavaproject.mudders.model.TownModel;
 @Repository
 public interface TownRepository extends CrudRepository<Town, Long> {
 
-
-
     Town findTownByTownName(String townName);
 
 }
