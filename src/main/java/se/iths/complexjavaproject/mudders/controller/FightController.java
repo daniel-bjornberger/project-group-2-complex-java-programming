ac248daf19a9adb563946da7ddbc828daee0efd8
Elin <elin.sexton@gmail.com>
2019-12-13 13:46:11 +0100

    Add FightController

---
+++ src/main/java/se/iths/complexjavaproject/mudders/controller/FightController.java
@@ -0,0 +1,46 @@
+package se.iths.complexjavaproject.mudders.controller;
+
+
+import lombok.NoArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import se.iths.complexjavaproject.mudders.entity.PlayerCharacter;
+import se.iths.complexjavaproject.mudders.repository.PlayerCharacterRepository;
+import se.iths.complexjavaproject.mudders.service.PlayerCharacterService;
+import se.iths.complexjavaproject.mudders.service.TravelService;
+import se.iths.complexjavaproject.mudders.service.World;
+
+import javax.websocket.server.PathParam;
+
+@RestController
+@NoArgsConstructor
+@RequestMapping("/fight")
+public class FightController {
+
+    @Autowired
+    private PlayerCharacterRepository playerCharacterRepository;
+
+    @Autowired
+    PlayerCharacterService playerCharacterService;
+
+    @Autowired
+    World world;
+
+    @Autowired
+    TravelService travelService;
+
+    @GetMapping(path = "choice")
+    public void playerCombatChoice(@PathParam("choice") String choice){
+        try {
+
+
+
+        }catch (Exception e){
+           
+        }
+    }
+
+}
