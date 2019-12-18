03393f6a38847f906c7121bd68c66ddf963630cb
Tonny Frisk <tonny035@hotmail.com>
2019-12-18 17:26:24 +0100

    add monster entity

--- src/main/java/se/iths/complexjavaproject/mudders/entity/Monster.java
+++ src/main/java/se/iths/complexjavaproject/mudders/entity/Monster.java
@@ -1,4 +1,26 @@
 package se.iths.complexjavaproject.mudders.entity;
 
-public class Monster {
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="monster")
+@Data
+public class Monster implements Serializable {
+
+    private static final long serialVersionUID = -1170333618087042027L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    //private List<Loot> monsterLoot;
+
 }
+
