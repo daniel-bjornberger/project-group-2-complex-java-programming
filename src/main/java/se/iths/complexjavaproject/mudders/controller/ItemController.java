600d53d560159796165908f8096b39cefcad581f
Daniel Björnberger <daniel.bjornberger@gmail.com>
2019-12-20 12:26:04 +0000

    Add the class 'ItemController'

---
+++ src/main/java/se/iths/complexjavaproject/mudders/controller/ItemController.java
@@ -0,0 +1,109 @@
+package se.iths.complexjavaproject.mudders.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import se.iths.complexjavaproject.mudders.entity.Item;
+import se.iths.complexjavaproject.mudders.model.ItemModel;
+import se.iths.complexjavaproject.mudders.service.ItemService;
+
+import javax.ws.rs.PathParam;
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/item")
+public class ItemController {
+
+    private final ItemService itemService;
+
+
+    @Autowired
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+
+    @GetMapping(path = "/getall")
+    public ResponseEntity getAllItems() {
+
+        try {
+            ArrayList<ItemModel> itemModels = new ArrayList<>();
+
+            itemService.getAllItems().forEach(item -> {
+                itemModels.add(item.toModel());
+            });
+
+            return ResponseEntity.ok().body(itemModels);
+
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
+    }
+
+
+    @PostMapping(path = "/add")
+    public ResponseEntity addItem(@RequestBody ItemModel itemModel) {
+
+        try {
+            Item item = itemModel.convertToEntity();
+
+            itemService.addItem(item);
+
+            return ResponseEntity.ok().body(itemModel);
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+        }
+
+    }
+
+
+    @GetMapping(path = "/getbyname/{name}")
+    public ResponseEntity getItemByName(@PathParam("name") String name) {
+
+        try {
+            return ResponseEntity.ok().body(itemService.getItemByName(name).toModel());
+
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
+    }
+
+
+    @PutMapping(path = "/updateitemvalue")
+    public ResponseEntity updateItemValue(@RequestBody ItemModel itemModel) {
+
+        try {
+            itemService.updateItemValue(itemModel.convertToEntity());
+
+            return ResponseEntity.ok().body(itemModel);
+
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
+    }
+
+
+    @DeleteMapping(path = "/deletebyname/{name}")
+    public ResponseEntity deleteItemByName(@PathParam("name") String name) {
+
+        try {
+            itemService.deleteItemByName(name);
+
+            return ResponseEntity.ok().build();
+
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+
+    }
+
+
+    /*@PostMapping(path = "/additemtoplayercharacter")
+    public ResponseEntity addItemToPlayerCharacter()*/
+
+}
