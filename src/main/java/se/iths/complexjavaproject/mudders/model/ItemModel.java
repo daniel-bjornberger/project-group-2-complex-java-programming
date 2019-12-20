d05b6871bb7d698feb2975a7152e27d5c86d7b23
Daniel Björnberger <daniel.bjornberger@gmail.com>
2019-12-20 12:24:48 +0000

    Move the method 'convertToEntity' to the class 'ItemModel'

--- src/main/java/se/iths/complexjavaproject/mudders/model/ItemModel.java
+++ src/main/java/se/iths/complexjavaproject/mudders/model/ItemModel.java
@@ -2,6 +2,8 @@ package se.iths.complexjavaproject.mudders.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import se.iths.complexjavaproject.mudders.entity.Item;
+import se.iths.complexjavaproject.mudders.exception.BadDataException;
 
 @Data
 @AllArgsConstructor
@@ -9,5 +11,15 @@ public class ItemModel {
 
     private String name;
     private int value;
+
+    public Item convertToEntity() throws BadDataException {
+
+        if (this.getName() == null || this.getName().isBlank()) {
+            throw new BadDataException("The name of the item is missing.");
+        }
+
+        return new Item(this.getName(), this.getValue());
+
+    }
 
 }
