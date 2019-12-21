a1c3ceeb2817f4dc35d62ab35911afe0d742b1c3
Tonny Frisk <tonny035@hotmail.com>
2019-12-21 14:20:34 +0100

    update playerService

--- src/main/java/se/iths/complexjavaproject/mudders/service/PlayerCharacterService.java
+++ src/main/java/se/iths/complexjavaproject/mudders/service/PlayerCharacterService.java
@@ -2,23 +2,29 @@ package se.iths.complexjavaproject.mudders.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import se.iths.complexjavaproject.mudders.entity.Town;
 import se.iths.complexjavaproject.mudders.exception.BadDataException;
 import se.iths.complexjavaproject.mudders.entity.PlayerCharacter;
+import se.iths.complexjavaproject.mudders.exception.PlayerNotFoundException;
 import se.iths.complexjavaproject.mudders.repository.PlayerCharacterRepository;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Service
 public class PlayerCharacterService {
 
-    @Autowired
-    PlayerCharacterRepository playerCharacterRepository;
+    private PlayerCharacterRepository playerCharacterRepository;
+    private TownService townService;
 
-    public static PlayerCharacter convertToEntity (String playerJson) throws BadDataException {
+    @Autowired
+    public PlayerCharacterService(PlayerCharacterRepository playerCharacterRepository, TownService townService) {
+        this.playerCharacterRepository = playerCharacterRepository;
+        this.townService = townService;
+    }
+
+    static PlayerCharacter convertToEntity(String playerJson) throws BadDataException {
         PlayerCharacter playerCharacter = new PlayerCharacter();
         ObjectMapper objectMapper = new ObjectMapper();
 
@@ -34,4 +40,24 @@ public class PlayerCharacterService {
         return playerCharacter;
     }
 
+    public PlayerCharacter findCharacterByName(String name) {
+        PlayerCharacter character = playerCharacterRepository.findByCharacterName(name);
+        if (character == null) {
+            throw new PlayerNotFoundException("Could not find character with the name: " + name);
+        }
+        return character;
+    }
+
+    // TODO test this and maybe changed to return a list
+    public Iterable<PlayerCharacter> findAll() {
+        return playerCharacterRepository.findAll();
+    }
+
+    public void createNewCharacter(String jsonData) throws BadDataException {
+        PlayerCharacter playerCharacter = convertToEntity(jsonData);
+        Town town = townService.findTown("First"); // TODO maybe change to use id
+        playerCharacter.setCurrentTown(town);
+        town.getPlayers().add(playerCharacter);
+        townService.saveTown(town); // town is the parent here, so when we save the town we also save the player.
+    }
 }
