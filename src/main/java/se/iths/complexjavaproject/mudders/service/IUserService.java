5c3381d8aad09d1d044c890b0e58115e93be1a81
Tonny Frisk <tonny035@hotmail.com>
2019-12-22 11:18:56 +0100

    Update userService interface

--- src/main/java/se/iths/complexjavaproject/mudders/service/IUserService.java
+++ src/main/java/se/iths/complexjavaproject/mudders/service/IUserService.java
@@ -1,11 +1,14 @@
 package se.iths.complexjavaproject.mudders.service;
 
 import se.iths.complexjavaproject.mudders.entity.User;
+import se.iths.complexjavaproject.mudders.exception.BadDataException;
 import se.iths.complexjavaproject.mudders.exception.EmailExistsException;
 import se.iths.complexjavaproject.mudders.model.UserModel;
 
 public interface IUserService {
 
     User registerNewUserAccount(UserModel userModel) throws EmailExistsException;
+
+    User findUserByEmail(String email) throws BadDataException;
 
 }
