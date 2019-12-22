48bf7a37003264107309105a7463a1b00e0a632e
Tonny Frisk <tonny035@hotmail.com>
2019-12-22 15:11:29 +0100

    update createNew Character to save user in character

--- src/main/java/se/iths/complexjavaproject/mudders/service/PlayerCharacterService.java
+++ src/main/java/se/iths/complexjavaproject/mudders/service/PlayerCharacterService.java
@@ -6,9 +6,11 @@ import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import se.iths.complexjavaproject.mudders.entity.Town;
+import se.iths.complexjavaproject.mudders.entity.User;
 import se.iths.complexjavaproject.mudders.exception.BadDataException;
 import se.iths.complexjavaproject.mudders.entity.PlayerCharacter;
 import se.iths.complexjavaproject.mudders.exception.PlayerNotFoundException;
+import se.iths.complexjavaproject.mudders.model.PlayerCharacterModel;
 import se.iths.complexjavaproject.mudders.repository.PlayerCharacterRepository;
 
 @NoArgsConstructor
@@ -17,11 +19,13 @@ public class PlayerCharacterService {
 
     private PlayerCharacterRepository playerCharacterRepository;
     private TownService townService;
+    private UserService userService;
 
     @Autowired
-    public PlayerCharacterService(PlayerCharacterRepository playerCharacterRepository, TownService townService) {
+    public PlayerCharacterService(PlayerCharacterRepository playerCharacterRepository, TownService townService, UserService userService) {
         this.playerCharacterRepository = playerCharacterRepository;
         this.townService = townService;
+        this.userService = userService;
     }
 
     static PlayerCharacter convertToEntity(String playerJson) throws BadDataException {
@@ -53,11 +57,20 @@ public class PlayerCharacterService {
         return playerCharacterRepository.findAll();
     }
 
-    public void createNewCharacter(String jsonData) throws BadDataException {
-        PlayerCharacter playerCharacter = convertToEntity(jsonData);
+    public PlayerCharacterModel createNewCharacter(String name, String email) throws BadDataException {
+        PlayerCharacter playerCharacter = new PlayerCharacter();
+        playerCharacter.setCharacterName(name);
+        if (playerCharacter.getCharacterName().isBlank()) {
+            throw new BadDataException("No name entered");
+        }
+        User user = userService.findUserByEmail(email);
         Town town = townService.findTown("First"); // TODO maybe change to use id
+        playerCharacter.setUserId(user);
         playerCharacter.setCurrentTown(town);
         town.getPlayers().add(playerCharacter);
         townService.saveTown(town); // town is the parent here, so when we save the town we also save the player.
+        user.getCharacters().add(playerCharacter);
+//        userService.saveUser(user); // user is parent.
+        return playerCharacter.toModel();
     }
 }
