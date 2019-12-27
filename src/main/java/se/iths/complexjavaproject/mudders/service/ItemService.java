5676a77af0ffd3bd4f04b353ef90127f9eef176e
Daniel Björnberger <daniel.bjornberger@gmail.com>
2019-12-27 10:24:51 +0000

    In the class 'ItemService', update the methods 'addItem', 'getItemByName' and 'updateItemValue' so that they return 'ItemModel' instead of 'Item' or 'void'. Also, implement the method 'addItemToPlayerCharacter'

--- src/main/java/se/iths/complexjavaproject/mudders/service/ItemService.java
+++ src/main/java/se/iths/complexjavaproject/mudders/service/ItemService.java
@@ -3,6 +3,13 @@ package se.iths.complexjavaproject.mudders.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import se.iths.complexjavaproject.mudders.entity.Item;
+import se.iths.complexjavaproject.mudders.entity.ItemAmount;
+import se.iths.complexjavaproject.mudders.entity.ItemAmountId;
+import se.iths.complexjavaproject.mudders.entity.PlayerCharacter;
+import se.iths.complexjavaproject.mudders.exception.BadDataException;
+import se.iths.complexjavaproject.mudders.exception.PlayerNotFoundException;
+import se.iths.complexjavaproject.mudders.model.ItemAmountModel;
+import se.iths.complexjavaproject.mudders.model.ItemModel;
 import se.iths.complexjavaproject.mudders.repository.ItemAmountRepository;
 import se.iths.complexjavaproject.mudders.repository.ItemRepository;
 import se.iths.complexjavaproject.mudders.repository.PlayerCharacterRepository;
@@ -37,7 +44,9 @@ public class ItemService {
     }
 
 
-    public void addItem(Item item) throws Exception {
+    public ItemModel addItem(ItemModel itemmodel) throws Exception {
+
+        Item item = itemmodel.convertToEntity();
 
         if (itemRepository.existsByName(item.getName())) {
             throw new Exception();
@@ -45,10 +54,12 @@ public class ItemService {
 
         itemRepository.save(item);
 
+        return itemmodel;
+
     }
 
 
-    public Item getItemByName(String name) throws Exception {
+    public ItemModel getItemByName(String name) throws Exception {
 
         Optional<Item> optionalItem = itemRepository.findByName(name);
 
@@ -56,12 +67,12 @@ public class ItemService {
             throw new Exception();
         }
 
-        return optionalItem.get();
+        return optionalItem.get().toModel();
 
     }
 
 
-    public void updateItemValue(Item item) throws Exception {
+    public ItemModel updateItemValue(ItemModel itemModel) throws Exception {
 
         /*if (itemRepository.existsByName(item.getName())) {
             Optional<Item> itemToUpdate = itemRepository.findByName(item.getName());
@@ -73,28 +84,83 @@ public class ItemService {
         }*/
 
 
-        Optional<Item> optionalItemToUpdate = itemRepository.findByName(item.getName());
+        // TODO: RETURNERA ItemModel?
+
+
+        Optional<Item> optionalItemToUpdate = itemRepository.findByName(itemModel.getName());
 
         if (optionalItemToUpdate.isEmpty()) {
             throw new Exception();
         }
 
         Item itemToUpdate = optionalItemToUpdate.get();
-        itemToUpdate.setValue(item.getValue());
+        itemToUpdate.setValue(itemModel.getValue());
         itemRepository.save(itemToUpdate);
 
+        return itemToUpdate.toModel();
+
     }
+
 
     public void deleteItemByName(String name) throws Exception {
 
         if (itemRepository.existsByName(name)) {
-
             itemRepository.deleteByName(name);
-
         }
         else {
             throw new Exception();
         }
+
+    }
+
+
+    public ItemAmountModel addItemToPlayerCharacter(String characterName, String itemName, int amount) throws Exception {
+
+        PlayerCharacter playerCharacter;
+        Optional<Item> optionalItem;
+        Item item;
+        Optional<ItemAmount> optionalItemAmount;
+        ItemAmount itemAmount;
+
+        if (characterName == null || itemName == null) {
+            throw new BadDataException("At least one of the required strings is missing.");
+        }
+
+        if (amount <= 0) {
+            throw new BadDataException("The amount is invalid.");
+        }
+
+        if (playerCharacterRepository.existsByCharacterName(characterName)) {
+            playerCharacter = playerCharacterRepository.findByCharacterName(characterName);
+        }
+        else {
+            throw new PlayerNotFoundException("A player character with the name '"
+                    + characterName + "' could not be found.");
+        }
+
+        optionalItem = itemRepository.findByName(itemName);
+
+        if (optionalItem.isEmpty()) {
+            throw new Exception();
+        }
+        else {
+            item = optionalItem.get();
+        }
+
+        optionalItemAmount = itemAmountRepository
+                .findById(new ItemAmountId(playerCharacter.getId(), item.getId()));
+
+        if (optionalItemAmount.isPresent()) {
+            itemAmount = optionalItemAmount.get();
+            itemAmount.setAmount(itemAmount.getAmount() + amount);
+        }
+        else {
+            itemAmount = new ItemAmount(playerCharacter, item, amount);
+        }
+
+        itemAmountRepository.save(itemAmount);
+
+        return itemAmount.toModel();
 
     }
 
