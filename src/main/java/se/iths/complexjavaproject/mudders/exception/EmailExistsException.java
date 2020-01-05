b384250098ebf2820be03047dd2aee4ab86afb9d
Elin <elin.sexton@gmail.com>
2020-01-05 17:05:09 +0100

    Add serialVersionUID

--- src/main/java/se/iths/complexjavaproject/mudders/exception/EmailExistsException.java
+++ src/main/java/se/iths/complexjavaproject/mudders/exception/EmailExistsException.java
@@ -1,6 +1,8 @@
 package se.iths.complexjavaproject.mudders.exception;
 
 public class EmailExistsException extends Exception {
+    private static final long serialVersionUID = 2718453335798281639L;
+
     public EmailExistsException(String message){
         super(message);
     }
