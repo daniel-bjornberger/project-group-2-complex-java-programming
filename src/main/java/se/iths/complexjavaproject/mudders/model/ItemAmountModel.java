12389a1e8292c3b0f627c003f2702549bc2619f8
Daniel Björnberger <daniel.bjornberger@gmail.com>
2020-01-08 21:55:23 +0000

    Add the annotation '@NoArgsConstructor' to the class 'ItemAmountModel'

--- src/main/java/se/iths/complexjavaproject/mudders/model/ItemAmountModel.java
+++ src/main/java/se/iths/complexjavaproject/mudders/model/ItemAmountModel.java
@@ -2,9 +2,11 @@ package se.iths.complexjavaproject.mudders.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ItemAmountModel {
 
     private String characterName;
