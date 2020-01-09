c1edb9efe3d06fac4976f10628c57ce506244e78
Elin <elin.sexton@gmail.com>
2020-01-09 11:30:35 +0100

    Add Shop

---
+++ src/main/java/se/iths/complexjavaproject/mudders/entity/Shop.java
@@ -0,0 +1,44 @@
+package se.iths.complexjavaproject.mudders.entity;
+
+import lombok.*;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Table(name = "shop")
+@NoArgsConstructor
+@ToString
+@Service
+public class Shop extends NonPlayerCharacter  {
+
+    private static final long serialVersionUID = 4552624858238223436L;
+
+    public void shopIntro(){
+        System.out.println("Welcome! How can I help you?");
+        System.out.println("I would like to buy some potions.");
+        System.out.println("Ah ofcourse! We are the finest purveyors of potions this side of Langaria.");
+    }
+
+    public PlayerCharacter shopping(PlayerCharacter playerCharacter){
+         int price = 5;
+
+         if(playerCharacter.getCurrency() >= price){
+             System.out.println("The price of a potion bottle is 5 coins.");
+             System.out.println("Yes it is a steep price but demand is always high.");
+             System.out.println("I have enough"+ playerCharacter.getCurrency());
+             playerCharacter.setCurrency(playerCharacter.getCurrency() - price);
+             //Add potion to inventory
+
+             System.out.println("Inventory now contains " + "playerCharacter.inventory.potions.amount" + " potions");
+             return playerCharacter;
+         }
+         else{
+             System.out.println("I can't afford that right now.");
+             System.out.println("Oh well, come again when you can.");
+             return playerCharacter;
+         }
+
+    }
+}
