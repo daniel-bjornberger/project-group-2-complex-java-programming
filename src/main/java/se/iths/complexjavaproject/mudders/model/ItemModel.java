ed5526d8e93f93fe89d676e123cf69b2ba08c13b
Daniel Björnberger <daniel.bjornberger@gmail.com>
2020-01-12 01:27:04 +0000

    Add the fields 'damage', 'healthRecovery' and 'maxAmount' to the class 'ItemModel'

--- src/main/java/se/iths/complexjavaproject/mudders/model/ItemModel.java
+++ src/main/java/se/iths/complexjavaproject/mudders/model/ItemModel.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import se.iths.complexjavaproject.mudders.entity.Item;
 import se.iths.complexjavaproject.mudders.exception.BadDataException;
 
+import java.util.ArrayList;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,6 +15,10 @@ public class ItemModel {
 
     private String name;
     private int value;
+    private int damage;
+    private int healthRecovery;
+    private int maxAmount;
+    //private ArrayList<ItemAmountModel> itemAmountModels = new ArrayList<>();
 
     public Item convertToEntity() throws BadDataException {
 
@@ -24,7 +30,22 @@ public class ItemModel {
             throw new BadDataException("The value can not be less than zero.");
         }
 
-        return new Item(this.getName(), this.getValue());
+        if (this.getDamage() < 0) {
+            throw new BadDataException("The damage can not be less than zero.");
+        }
+
+        if (this.getHealthRecovery() < 0) {
+            throw new BadDataException("The health recovery can not be less than zero.");
+        }
+
+        if (this.getMaxAmount() < 1) {
+            throw new BadDataException("The maximum amount can not be less than one.");
+        }
+
+        // TODO: Max-värden på damage, healthRecovery och maxAmount?
+
+        return new Item(this.getName(), this.getValue(),
+                this.getDamage(), this.getHealthRecovery(), this.getMaxAmount());
 
     }
 
