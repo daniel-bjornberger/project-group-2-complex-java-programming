b719ae64db6a4d3b4419b943eed9ed4410a649fd
Daniel Björnberger <daniel.bjornberger@gmail.com>
2020-01-12 00:41:00 +0000

    Add the fields 'damage', 'healthRecovery' and 'maxAmount' to the entity/class 'Item'

--- src/main/java/se/iths/complexjavaproject/mudders/entity/Item.java
+++ src/main/java/se/iths/complexjavaproject/mudders/entity/Item.java
@@ -1,11 +1,13 @@
 package se.iths.complexjavaproject.mudders.entity;
 
 import lombok.*;
+import se.iths.complexjavaproject.mudders.model.ItemAmountModel;
 import se.iths.complexjavaproject.mudders.model.ItemModel;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -29,18 +31,37 @@ public class Item implements Serializable {
     @Column(name = "value")
     private int value;
 
+    @Column(name = "damage")
+    private int damage;
+
+    @Column(name = "health_recovery")
+    private int healthRecovery;
+
+    @Column(name = "max_amount")
+    private int maxAmount;
+
     @OneToMany(mappedBy = "item", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<ItemAmount> itemAmounts = new HashSet<>();
 
 
-    public Item(String name, int value) {
-        this.name = name;
-        this.value = value;
+    public Item(String name, int value, int damage, int healthRecovery, int maxAmount) {
+        this.name           = name;
+        this.value          = value;
+        this.damage         = damage;
+        this.healthRecovery = healthRecovery;
+        this.maxAmount      = maxAmount;
     }
 
 
     public ItemModel toModel() {
-        return new ItemModel(this.name, this.value);
+
+        //ArrayList<ItemAmountModel> itemAmountModels = new ArrayList<>();
+
+        //this.itemAmounts.forEach(itemAmount -> itemAmountModels.add(itemAmount.toModel()));
+
+        return new ItemModel(this.name, this.value,
+                this.damage, this.healthRecovery, this.maxAmount);
+
     }
 
 }
