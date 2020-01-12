8fbda1262cb5d74e20a5a83c1b80dcace9066694
Daniel Björnberger <daniel.bjornberger@gmail.com>
2020-01-12 00:25:34 +0000

    Introduce the correct exceptions instead of 'Exception', in the class 'ItemController'

--- src/main/java/se/iths/complexjavaproject/mudders/controller/ItemController.java
+++ src/main/java/se/iths/complexjavaproject/mudders/controller/ItemController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import se.iths.complexjavaproject.mudders.exception.*;
 import se.iths.complexjavaproject.mudders.model.ItemAmountModel;
 import se.iths.complexjavaproject.mudders.model.ItemModel;
 import se.iths.complexjavaproject.mudders.service.ItemService;
@@ -22,16 +23,9 @@ public class ItemController {
         this.itemService = itemService;
     }
 
-    /*@Data
-    private static class CharacterAndItemWrapper {
-        private String characterName;
-        private String itemName;
-        private int amount;
-    }*/
-
 
     @GetMapping(path = "/getall")
-    public ResponseEntity getAllItems() {
+    public ResponseEntity<?> getAllItems() {
 
         try {
             ArrayList<ItemModel> itemModels = new ArrayList<>();
@@ -39,60 +33,58 @@ public class ItemController {
             itemService.getAllItems().forEach(item -> itemModels.add(item.toModel()));
 
             return ResponseEntity.ok().body(itemModels);
-
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
 
     }
 
 
     @PostMapping(path = "/add")
-    public ResponseEntity addItem(@RequestBody ItemModel itemModel) {
+    public ResponseEntity<?> addItem(@RequestBody ItemModel itemModel) {
 
         try {
-            //Item item = itemModel.convertToEntity();
-            //itemService.addItem(itemModel);
-
             return ResponseEntity.ok().body(itemService.addItem(itemModel));
-
-        } catch (Exception e) {
+        }
+        catch (DuplicateItemException e) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+        }
+        catch (BadDataException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
 
     }
 
 
     @GetMapping(path = "/getbyname/{name}")
-    public ResponseEntity getItemByName(@PathVariable("name") String name) {
+    public ResponseEntity<?> getItemByName(@PathVariable("name") String name) {
 
         try {
             return ResponseEntity.ok().body(itemService.getItemByName(name));
-
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        catch (ItemNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
 
     }
 
 
-    @PutMapping(path = "/updateitemvalue")
-    public ResponseEntity updateItemValue(@RequestBody ItemModel itemModel) {
+    @PutMapping(path = "/updateitemdata")
+    public ResponseEntity<?> updateItemData(@RequestBody ItemModel itemModel) {
 
         try {
-            //itemService.updateItemValue(itemModel);
-
-            return ResponseEntity.ok().body(itemService.updateItemValue(itemModel));
-
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            return ResponseEntity.ok().body(itemService.updateItemData(itemModel));
+        }
+        catch (ItemNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
 
     }
 
 
     @DeleteMapping(path = "/deletebyname/{name}")
-    public ResponseEntity deleteItemByName(@PathVariable("name") String name) {
+    public ResponseEntity<?> deleteItemByName(@PathVariable("name") String name) {
 
         try {
             itemService.deleteItemByName(name);
@@ -100,53 +92,61 @@ public class ItemController {
             // TODO: Behöver man kolla om en item ingår i en itemAmount, innan man deletar den?
 
             return ResponseEntity.ok().build();
-
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().build();
+        }
+        catch (ItemNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
 
     }
 
 
     @PostMapping(path = "/additemtoplayercharacter")
-    public ResponseEntity addItemToPlayerCharacter(@RequestBody ItemAmountModel itemAmountModel) {
+    public ResponseEntity<?> addItemToPlayerCharacter(@RequestBody ItemAmountModel itemAmountModel) {
 
         try {
-            /*itemService.addItemToPlayerCharacter(itemAmountModel);*/
             return ResponseEntity.ok().body(itemService.addItemToPlayerCharacter(itemAmountModel));
-
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().build();
+        }
+        catch (PlayerNotFoundException | ItemNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+        catch (BadDataException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
 
     }
 
 
     @PostMapping(path = "/removeitemfromplayercharacter")
-    public ResponseEntity removeItemFromPlayerCharacter(@RequestBody ItemAmountModel itemAmountModel) {
+    public ResponseEntity<?> removeItemFromPlayerCharacter(@RequestBody ItemAmountModel itemAmountModel) {
 
         try {
             return ResponseEntity.ok().body(itemService.removeItemFromPlayerCharacter(itemAmountModel));
-
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().build();
+        }
+        catch (PlayerNotFoundException | ItemNotFoundException | ItemAmountNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+        catch (BadDataException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
 
     }
 
 
-    @GetMapping(path = "/getitemamount")
-    public ResponseEntity getItemAmount(@RequestParam("charactername") String characterName,
+    /*@GetMapping(path = "/getitemamount")
+    public ResponseEntity<?> getItemAmount(@RequestParam("charactername") String characterName,
                                         @RequestParam("itemname") String itemName) {
 
         try {
             return ResponseEntity.ok()
                     .body(itemService.getItemAmount(characterName, itemName));
-
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        catch (PlayerNotFoundException | ItemNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+        catch (BadDataException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
 
-    }
+    }*/
 
 }
