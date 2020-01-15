a2a0d020e051b195b5a4dbc123ce863ecd948049
Tonny <tonny035@hotmail.com>
2020-01-15 10:47:36 +0100

    update Role entity

--- src/main/java/se/iths/complexjavaproject/mudders/entity/Role.java
+++ src/main/java/se/iths/complexjavaproject/mudders/entity/Role.java
@@ -1,4 +1,25 @@
 package se.iths.complexjavaproject.mudders.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Data
+@Entity
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToMany(mappedBy = "roles")
+    private Collection<User> users;
+
+    @ManyToMany
+    @JoinTable(name = "roles_privileges", joinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "privilege_id", referencedColumnName = "id"))
+    private Collection<Privilege> privileges;
+
+    private String name;
+
 }
