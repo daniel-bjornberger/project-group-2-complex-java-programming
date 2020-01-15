5ee833557519341c9948a3a67d07b0529f43b6af
Tonny <tonny035@hotmail.com>
2020-01-15 10:49:48 +0100

    update Privilege entity

--- src/main/java/se/iths/complexjavaproject/mudders/entity/Privilege.java
+++ src/main/java/se/iths/complexjavaproject/mudders/entity/Privilege.java
@@ -1,4 +1,21 @@
 package se.iths.complexjavaproject.mudders.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Data
+@Entity
 public class Privilege {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "privileges")
+    private Collection<Role> roles;
+    
 }
